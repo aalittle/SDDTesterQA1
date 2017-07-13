7047a0aaff22008a7f5739471274bdf591fc3c04
Andrew Little <alittle@salesforce.com>
2017-07-13 09:14:12 -0300

    first commit

---
+++ SDK Tester QA1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDK Tester QA1
+//
+//  Created by Andrew Little on 7/13/17.
+//  Copyright © 2017 Andrew Little. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
